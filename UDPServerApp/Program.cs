47bb018aad5230554a3baef4a5f8d24932d00ffe
Ungraded <54710331+Ungraded@users.noreply.github.com>
2020-06-08 00:35:15 +0300

    Add project files.

---
+++ UDPServerApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        IPAddress broadcast = IPAddress.Parse("127.0.0.1");
+
+        //byte[] sendbuf = Encoding.UTF8.GetBytes(args[0]);
+        byte[] sendbuf = Encoding.Default.GetBytes(DateTime.Now.ToString());
+        IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+
+        do
+        {
+            sendbuf = Encoding.Default.GetBytes(DateTime.Now.ToString());
+            s.SendTo(sendbuf, ep);
+
+            Console.WriteLine("Message sent to the broadcast address");
+
+            int milliseconds = 5000;
+            Thread.Sleep(milliseconds);
+        } while (true);
+
+    }
+}
